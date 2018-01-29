3cc9523a8b3a29c50e3f8f29026de913cb8373be
piyo7 <piyo7@users.noreply.github.com>
2018-01-29 23:05:07 +0900

    simplify Util.bitsSize implementation

--- src/main/scala/com/github/piyo7/qlione/Util.scala
+++ src/main/scala/com/github/piyo7/qlione/Util.scala
@@ -1,20 +1,11 @@
 package com.github.piyo7.qlione
 
-import scala.annotation.tailrec
-
 object Util {
 
   implicit class RichInt(val i: Int) extends AnyVal {
     def pow2: Int = math.pow(2, i).round.toInt
 
-    def bitsSize: Int = bitsSize(0)
-
-    @tailrec
-    private def bitsSize(r: Int): Int = i match {
-      case _ if i <= 0 => 0
-      case 1 => r + 1
-      case _ => (i / 2).bitsSize(r + 1)
-    }
+    def bitsSize: Int = i.toBinaryString.length
 
     def bitsString(size: Int): String = ("0" * size + i.toBinaryString).takeRight(size)
   }
